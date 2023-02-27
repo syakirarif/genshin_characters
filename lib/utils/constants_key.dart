34330ae7ce761b17ab220a03ff6ec59c9e248b37
Syakir Arif <muhsyakirarif12@gmail.com>
2023-02-27 11:44:02 +0700

    Added Dehya & Mika | Updated app version

---
+++ lib/utils/constants_key.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+final _adUnitIdBannerTest = Platform.isAndroid
+    ? 'ca-app-pub-3940256099942544/6300978111'
+    : 'ca-app-pub-3940256099942544/2934735716';
+
+const _adUnitIdBannerProd = 'ca-app-pub-9167080444329708/1542151532';
+const adUnitIdBanner = _adUnitIdBannerProd;
+
+/////////////////////////////////////////////
+
+final _adUnitIdInterstitialTest = Platform.isAndroid
+    ? 'ca-app-pub-3940256099942544/1033173712'
+    : 'ca-app-pub-3940256099942544/4411468910';
+
+const _adUnitIdInterstitialProd = 'ca-app-pub-9167080444329708/2141751488';
+const adUnitIdInterstitial = _adUnitIdInterstitialProd;
+
+/////////////////////////////////////////////
