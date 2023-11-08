ad4755d2708d2b43c317bb51888603c4c2aec335
Syakir Arif <muhsyakirarif12@gmail.com>
2023-11-08 14:25:15 +0700

    Changet print() to debugPrint()

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,12 +17,12 @@ import 'package:get/get.dart';
 @pragma('vm:entry-point')
 void notificationTapBackground(NotificationResponse notificationResponse) {
   // ignore: avoid_print
-  print('notification(${notificationResponse.id}) action tapped: '
+  debugPrint('notification(${notificationResponse.id}) action tapped: '
       '${notificationResponse.actionId} with'
       ' payload: ${notificationResponse.payload}');
   if (notificationResponse.input?.isNotEmpty ?? false) {
     // ignore: avoid_print
-    print(
+    debugPrint(
         'notification action tapped with input: ${notificationResponse.input}');
   }
 }
