b6990525734f290ad565468e504167429e2bc8e4
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-05 10:03:21 +0700

    Implement show the ad and handle fullscreen callbacks

--- lib/utils/AppOpenAdManager.dart
+++ lib/utils/AppOpenAdManager.dart
@@ -28,6 +28,38 @@ class AppOpenAdManager {
     );
   }
 
+  void showAdIfAvailable() {
+    if (!isAdAvailable) {
+      print('Tried to show ad before available.');
+      loadAd();
+      return;
+    }
+    if (_isShowingAd) {
+      print('Tried to show ad while already showing an ad.');
+      return;
+    }
+    // Set the fullScreenContentCallback and show the ad.
+    _appOpenAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (ad) {
+        _isShowingAd = true;
+        print('$ad onAdShowedFullScreenContent');
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        _isShowingAd = false;
+        ad.dispose();
+        _appOpenAd = null;
+      },
+      onAdDismissedFullScreenContent: (ad) {
+        print('$ad onAdDismissedFullScreenContent');
+        _isShowingAd = false;
+        ad.dispose();
+        _appOpenAd = null;
+        loadAd();
+      },
+    );
+  }
+
   /// Whether an ad is available to be shown.
   bool get isAdAvailable {
     return _appOpenAd != null;
